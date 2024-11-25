f3601c126a2e5145f87dd5be1966ca81af409ac9
EsteeFrei <e533163610@gmail.com>
2024-11-25 15:53:30 +0200

    add login and signup - back api

--- src/app/actions/userActions.ts
+++ src/app/actions/userActions.ts
@@ -1,11 +1,34 @@
-export const loginUser=()=>{
+import axios from "axios";
+import IUser from "../types/IUser";
 
+export const loginUser = async(username: string, password: string) => {
+    try {
+        const response = await axios.post('/api/user/login', { username, password });
+        return response.data;  
+    } catch (error) {
+        
+        if (error) {
+            return { error};
+        } else {
+            return { error: 'Something went wrong. Please try again.'};
+        }
+    }
 }
 
-export const signupUser=()=>{
-    
+export const signupUser = async(user: IUser) => {
+    try {
+        const response = await axios.post('/api/user/signup', { user});
+        return response.data;  
+    } catch (error) {
+        
+        if (error) {
+            return { error};
+        } else {
+            return { error: 'Something went wrong. Please try again.' };
+        }
+    }
 }
 
-export const resetPassword=()=>{
-    
+export const resetPassword = () => {
+
 }
---
+++ src/app/api/user/signup/route.ts
@@ -0,0 +1,43 @@
+export const dynamic = 'force-dynamic';
+import { connectToDB, disconnectFromDB } from "@/app/DB/connection/connectToDB";
+import User from "@/app/DB/models/UserModel";
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+
+    try {
+        await connectToDB();
+
+        const body = await req.json();
+        if (!body) {
+            return NextResponse.json({ message: "Missing body" }, { status: 400 });
+        }
+
+        // const parsedData = userSchema.parse(body);
+        // const newUser = new User(parsedData);
+
+        const newUser = new User(body);
+        await newUser.save();
+
+        console.log("created", newUser);
+
+        // מחזיר רק מידע בטוח
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { password, ...userWithoutPassword } = newUser.toObject();
+        return NextResponse.json(
+            { message: "User created successfully", user: userWithoutPassword },
+            { status: 201 }
+        );
+
+    } catch (err) {
+        console.error("Error creating user:", err);
+        return NextResponse.json(
+            { message: "Server error creating user", error: err},
+            { status: 500 }
+        );
+    } finally {
+        await disconnectFromDB();
+    }
+
+}
+
