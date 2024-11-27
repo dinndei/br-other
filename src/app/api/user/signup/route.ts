4d0843be47a95252f762563c4111be7244d1e2dd
dinndei <150654714+dinndei@users.noreply.github.com>
2024-11-27 14:01:43 +0200

    encrypt and decrypt sensitive data

--- src/app/api/user/signup/route.ts
+++ src/app/api/user/signup/route.ts
@@ -1,6 +1,8 @@
 export const dynamic = 'force-dynamic';
-import { connectToDB, disconnectFromDB } from "@/app/DB/connection/connectToDB";
+import { connectToDB } from "@/app/DB/connection/connectToDB";
 import User from "@/app/DB/models/UserModel";
+import { decryptData } from "@/app/lib/dataEncryption/decryptData";
+import { encryptData } from "@/app/lib/dataEncryption/encryptData";
 import { hashPassword } from "@/app/lib/passwordHash/hashPassword";
 import { NextRequest, NextResponse } from 'next/server';
 
@@ -18,11 +20,13 @@ export async function POST(req: NextRequest) {
         // const newUser = new User(parsedData);
 
         const hashedPassword = await hashPassword(body.password);
+        const encryptedType =  encryptData(String(body.type));
         
         // יצירת אובייקט המשתמש עם הסיסמה המוצפנת
         const newUser = new User({ 
             ...body, 
-            password: hashedPassword // הסיסמה המוצפנת
+            password: hashedPassword, // הסיסמה המוצפנת
+            type:encryptedType//הצפנת השתייכות המשתמש
         });
 
         await newUser.save();
@@ -31,7 +35,9 @@ export async function POST(req: NextRequest) {
 
         // מחזיר רק מידע בטוח
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { password, ...userWithoutPassword } = newUser.toObject();
+        const { password , ...userWithoutPassword } = newUser.toObject();
+        //המרה של הטייפ למידע לא מוצפן
+        userWithoutPassword.typeUser=JSON.parse(decryptData(JSON.stringify(userWithoutPassword.typeUser)))
         return NextResponse.json(
             { message: "User created successfully", user: userWithoutPassword },
             { status: 201 }
@@ -43,8 +49,6 @@ export async function POST(req: NextRequest) {
             { message: "Server error creating user", error: err},
             { status: 500 }
         );
-    } finally {
-        await disconnectFromDB();
     }
 
 }
