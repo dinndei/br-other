f6f36ddf6b760535756bcbe1b536a358f622b218
dinndei <150654714+dinndei@users.noreply.github.com>
2024-12-17 10:46:32 +0200

    sort by dist function

--- src/app/lib/sortByMatch/sortByDistance.ts
+++ src/app/lib/sortByMatch/sortByDistance.ts
@@ -1,14 +1,22 @@
 import IUser from "@/app/types/IUser";
+import { calculatePoliticGap, calculateReligionGap } from "./calculateGaps";
 
 // Function to calculate the total gap
 export const sortByDistance = (user: IUser, mentors: IUser[]): IUser[] => {
-    const sortedMentors = mentors.sort((a, b) => {
-        return religionLevelValues[b.religionLevel] - religionLevelValues[a.religionLevel];
-    });
-   return sortedMentors;
-}
+    const mentorGaps = mentors.map(mentor => ({
+        mentor,
+        dist: calculatePoliticGap(
+            user.typeUser.politicalAffiliation,
+            mentor.typeUser.politicalAffiliation
+        ) + calculateReligionGap(
+            user.typeUser.religionLevel,
+            mentor.typeUser.religionLevel
+        ),
+    }));
 
-// Sort the array from largest to smallest religion level
-const sortedPeople = people.sort((a, b) => {
-    return religionLevelValues[b.religionLevel] - religionLevelValues[a.religionLevel];
-});
+    // Sort mentors by distance
+    mentorGaps.sort((a, b) => a.dist - b.dist);
+
+    // Return sorted mentors
+    return mentorGaps.map(item => item.mentor);
+}
