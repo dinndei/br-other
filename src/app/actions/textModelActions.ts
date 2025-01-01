acfc99a25d748dc23968a86c09c520064e8d8de8
dinndei <150654714+dinndei@users.noreply.github.com>
2025-01-01 14:17:46 +0200

    fix vercel errors 2

--- src/app/actions/textModelActions.ts
+++ src/app/actions/textModelActions.ts
@@ -47,17 +47,18 @@ export const isToxic = async (text: string): Promise<boolean> => {
         
         
         return true; // ערך סף לזיהוי רעילות (ניתן לשנות בהתאם לצורך)
-    } catch (error) {
-        if (error.response && error.response.status === 503) {
-            console.log("Model loading, retrying in a few seconds");
-            return new Promise((resolve) => {
-                setTimeout(async () => {
-                    resolve(await isToxic(text));
-                },  2000);
-            });
-        } else {
-            console.error("Error:", error);
-            return false; // במקרה של שגיאה, מחזיר false
+    } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+            if (error.response && error.response.status === 503) {
+                console.log("Model loading, retrying in a few seconds");
+                return new Promise((resolve) => {
+                    setTimeout(async () => {
+                        resolve(await isToxic(text));
+                    }, 2000);
+                });
+            }
         }
+        console.error("Error:", error);
+        return false;
     }
 };
