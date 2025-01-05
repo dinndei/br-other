b0bfefa88bf0701249c1185133480e8521bdd15b
dinndei <150654714+dinndei@users.noreply.github.com>
2025-01-05 09:29:56 +0200

    2

--- src/app/api/ablyChat/[courseId]/route.ts
+++ src/app/api/ablyChat/[courseId]/route.ts
@@ -13,7 +13,7 @@ export async function GET(req: NextRequest, { params }: { params: { courseId: st
         await connectToDB();
         
         // שליפה של ההודעות לפי courseId
-        const messages = await ChatMessage.find({ courseId }).sort({ timestamp: -1 });
+        const messages = await ChatMessage.find({ courseId }).sort({ timestamp: 1 });
 
         if (messages.length === 0) {
             return NextResponse.json({ message: "No messages found for this courseId" }, { status: 404 });
